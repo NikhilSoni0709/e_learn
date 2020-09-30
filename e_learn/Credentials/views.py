a835f6d06c82b05998062d17b67fef511be1141f
Nikhil Soni <soninikhil665@gmail.com>
2020-09-30 23:21:07 +0530

    Credentials Done, Database Done

--- e_learn/Credentials/views.py
+++ e_learn/Credentials/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 def showLoginPage(request):
@@ -6,8 +8,14 @@ def showLoginPage(request):
         user_name = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
-        # print(user_name, password)
-        return redirect('home')
+
+        user = auth.authenticate(username=user_name, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Invalid username or password')
+            return redirect('Login')
     else:
         return render(request, 'Login.html')
 
@@ -20,7 +28,20 @@ def showRegisterPage(request):
         email = request.POST['email']
         pass_1 = request.POST['password1']
         pass_2 = request.POST['password2']
-        # print(f_name, user_name, pass_1)
-        return redirect('Login')
+
+        if(not User.objects.filter(username=user_name).exists()):
+            if(pass_1 != pass_2):
+                messages.info(request, "Passwords don't match")
+                return redirect('Register')
+            elif(User.objects.filter(email=email).exists()):
+                messages.info(request, 'Email already registered')
+                return redirect('Register')
+            else:
+                user = User.objects.create_user(first_name=f_name, last_name=l_name, username=user_name, password=pass_1, email=email)
+                user.save()
+                return redirect('Login')
+        else:
+            messages.info(request, 'Username Already taken')
+            return redirect('Register')
     else:
         return render(request, 'Register.html')
